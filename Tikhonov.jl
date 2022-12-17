20630c0a1a7cb32d3bf4a8d1d9a66c1e1a0c1f83
Christian Singer <christian.singer@mail.schwarz>
2022-12-17 10:16:08 +0100

    Update Tikhonov

--- Tikhonov.jl
+++ Tikhonov.jl
@@ -43,15 +43,16 @@ plot!(pt, [f_(i/n) for i = 1:n], label="f(x)")
 ### c)
 
 # Generate noisy data by adding uniform noise from [-0.01, 0.01] to g.
-g_noisy = g + 0.01 * (rand(n) .*2 .- 1)
+noise_level = 0.1
+g_noisy = g + noise_level * (rand(n) .*2 .- 1)
 
 # Calculate the approximation of g for different values of γ.
 g_approx = [B*f(γ, g_noisy) for γ = [0.1, 0.001, 0.0001]]
 # Plot g_approx and g for comparison.
 pt = plot(g_approx, label=["γ = 0.1" "γ = 0.001" "γ = 0.0001"],
                 linestyle = [:solid :dot :dashdotdot],
-                title="Tikhonov-Approximation of g(x)",
+                title="Tikhonov-Approximation of g(x) plus noise (ε=$(noise_level))",
                 xlabel="x",
                 ylabel="g(x)")
-plot!(pt, g, label="g(x)")
-plot!(pt, g_noisy, label="g(x) + ")
+plot!(pt, g, label="g(x)", linestyle=:dashdot)
+plot!(pt, g_noisy, label="g(x) + ε", linestyle=:dashdotdot)
